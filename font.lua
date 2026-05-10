099e51000ca4048bd80ef2ec6aadd17232ab763c
Tntfrog <tntfrog3030@gmail.com>
2026-05-10 13:11:18 -0500

    Create font.lua

---
+++ font.lua
@@ -0,0 +1,10 @@
+local FontModule = {}
+
+FontModule.Fonts = {
+	Gotham = Enum.Font.Gotham,
+	Bold = Enum.Font.GothamBold,
+	Code = Enum.Font.Code,
+	SourceSans = Enum.Font.SourceSans,
+}
+
+return FontModule
